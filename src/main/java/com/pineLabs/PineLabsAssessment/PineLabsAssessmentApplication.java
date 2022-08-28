75fcfe3522920038dc8bbd37ebe2c797b93fc0a2
Muaz Wazir <muazwazir@gmail.com>
2022-08-28 12:52:14 +0800

    First commit

---
+++ src/main/java/com/pineLabs/PineLabsAssessment/PineLabsAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.pineLabs.PineLabsAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineLabsAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineLabsAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pineLabs/PineLabsAssessment/PineLabsAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pineLabs.PineLabsAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineLabsAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

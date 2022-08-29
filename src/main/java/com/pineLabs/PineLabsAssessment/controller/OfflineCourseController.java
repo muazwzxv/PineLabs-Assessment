0461e81ae503c512123fbe4477135884aed972f2
Muaz Wazir <muazwazir@gmail.com>
2022-08-29 15:43:27 +0800

    add getById controller method & refactor Course not found exception

--- src/main/java/com/pineLabs/PineLabsAssessment/controller/OfflineCourseController.java
+++ src/main/java/com/pineLabs/PineLabsAssessment/controller/OfflineCourseController.java
@@ -1,17 +1,12 @@
 package com.pineLabs.PineLabsAssessment.controller;
 
-import com.pineLabs.PineLabsAssessment.model.AbstractModel;
-import com.pineLabs.PineLabsAssessment.model.CourseOffline;
 import com.pineLabs.PineLabsAssessment.service.ICourseOfflineService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/courses")
@@ -22,15 +17,25 @@ public class OfflineCourseController {
 
     @GetMapping
     public ResponseEntity<?> getAll(@RequestParam(name = "status") String status) {
-       switch (status) {
-           case "offline":
-               return new ResponseEntity<>(this.courseOfflineService.findAll(), HttpStatus.OK);
-           case "online":
-               break;
-           default:
-               break;
-       }
-       System.out.println(status);
-       return null;
+        switch (status) {
+            case "offline":
+                return new ResponseEntity<>(this.courseOfflineService.findAll(), HttpStatus.OK);
+            case "online":
+                return null;
+            default:
+                throw new RuntimeException("Invalid arguments");
+        }
+    }
+
+    @GetMapping("/{uid}")
+    public ResponseEntity<?> getById(@PathVariable("uid") String uid, @RequestParam(name = "status") String status) {
+        switch (status) {
+            case "offline":
+                return new ResponseEntity<>(this.courseOfflineService.findById(UUID.fromString(uid)), HttpStatus.OK);
+            case "online":
+                return null;
+            default:
+                throw new RuntimeException("Invalid arguments");
+        }
     }
 }
--- src/main/java/com/pineLabs/PineLabsAssessment/exception/CourseNotFoundException.java
+++ src/main/java/com/pineLabs/PineLabsAssessment/exception/CourseNotFoundException.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class CourseNotFoundException extends RuntimeException{
+public class CourseNotFoundException extends RuntimeException {
     private final String fieldName;
     private final Object fieldValue;
 
     public CourseNotFoundException(String fieldName, Object fieldValue) {
-        super(String.format("Family not found with %s: '%s'", fieldName, fieldValue));
+        super(String.format("Course not found with %s: '%s'", fieldName, fieldValue));
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
     }

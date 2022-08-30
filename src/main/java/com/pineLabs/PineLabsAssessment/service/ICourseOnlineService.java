61c41f13d3113672d93fb8ba88bd4193822f0a0c
Fortune-04 <62495862+Fortune-04@users.noreply.github.com>
2022-08-31 01:08:01 +0800

    Update ICourseOnlineService.java

--- src/main/java/com/pineLabs/PineLabsAssessment/service/ICourseOnlineService.java
+++ src/main/java/com/pineLabs/PineLabsAssessment/service/ICourseOnlineService.java
@@ -1,10 +1,17 @@
 package com.pineLabs.PineLabsAssessment.service;
 
 import com.pineLabs.PineLabsAssessment.model.CourseOnline;
+import com.pineLabs.PineLabsAssessment.request.CreateOnlineCourseRequest;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ICourseOnlineService extends ICrudService<CourseOnline, UUID> {
+public interface ICourseOnlineService extends IService<CourseOnline, UUID> {
     List<CourseOnline> findAllActiveCourses();
+
+    CourseOnline create(CreateOnlineCourseRequest request);
+
+    Optional<CourseOnline> updateById(CourseOnline updatedCourse, UUID uuid);
+
 }

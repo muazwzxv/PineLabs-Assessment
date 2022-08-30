a5e1156b88480176a8d306fe1629598955cf1218
Fortune-04 <62495862+Fortune-04@users.noreply.github.com>
2022-08-31 01:08:19 +0800

    Update IService.java

--- src/main/java/com/pineLabs/PineLabsAssessment/service/IService.java
+++ src/main/java/com/pineLabs/PineLabsAssessment/service/IService.java
@@ -1,11 +1,14 @@
 package com.pineLabs.PineLabsAssessment.service;
 
+import com.pineLabs.PineLabsAssessment.model.CourseOnline;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface IService<T, ID> {
     T findById(ID uid);
 
     List<T> findAll();
 
-    void deleteById(ID uid);
+    Optional<CourseOnline> deleteById(ID uid);
 }

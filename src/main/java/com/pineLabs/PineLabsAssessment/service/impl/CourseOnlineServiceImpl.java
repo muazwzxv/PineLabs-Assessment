5e8dd5de75a1b10553986a89e45cb9e6185e3e78
Fortune-04 <62495862+Fortune-04@users.noreply.github.com>
2022-08-31 01:08:14 +0800

    Update CourseOnlineServiceImpl.java

--- src/main/java/com/pineLabs/PineLabsAssessment/service/impl/CourseOnlineServiceImpl.java
+++ src/main/java/com/pineLabs/PineLabsAssessment/service/impl/CourseOnlineServiceImpl.java
@@ -4,8 +4,10 @@ import com.pineLabs.PineLabsAssessment.exception.CourseNotFoundException;
 import com.pineLabs.PineLabsAssessment.model.CourseOnline;
 import com.pineLabs.PineLabsAssessment.model.enums.CourseStatus;
 import com.pineLabs.PineLabsAssessment.repository.CourseOnlineRepository;
+import com.pineLabs.PineLabsAssessment.request.CreateOnlineCourseRequest;
 import com.pineLabs.PineLabsAssessment.service.ICourseOnlineService;
 import lombok.AllArgsConstructor;
+import lombok.var;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,11 +31,43 @@ public class CourseOnlineServiceImpl implements ICourseOnlineService {
         return this.courseOnlineRepository.findAll();
     }
 
-    public void deleteById(UUID uid){
+    public Optional<CourseOnline> deleteById(UUID uid){
+
+        var deleteCourse = this.courseOnlineRepository.findById(uid);
         this.courseOnlineRepository.deleteById(uid);
+        return deleteCourse;
     }
 
     public List<CourseOnline> findAllActiveCourses(){
         return this.courseOnlineRepository.getCourseOnlineByStatus(CourseStatus.ACTIVE);
+    }
+
+    public CourseOnline create(CreateOnlineCourseRequest requests){
+        CourseOnline course = CourseOnline.builder()
+                .courseName(requests.getCourseName())
+                .category(requests.getCategory())
+                .status(CourseStatus.PENDING)
+                .description(requests.getDescription())
+                .link(requests.getLink())
+                .instructorName(requests.getInstructorName())
+                .total_student(requests.getTotalStudent())
+                .build();
+        return this.courseOnlineRepository.save(course);
+    }
+
+    public Optional<CourseOnline> updateById(CourseOnline updatedCourse, UUID uuid){
+        return this.courseOnlineRepository.findById(uuid)
+                .map(courseOnline -> {
+                    courseOnline.setCourseName(updatedCourse.getCourseName());
+                    courseOnline.setCategory(updatedCourse.getCategory());
+                    courseOnline.setStatus(updatedCourse.getStatus());
+                    courseOnline.setDescription(updatedCourse.getDescription());
+                    courseOnline.setLink(updatedCourse.getLink());
+                    courseOnline.setInstructorName(updatedCourse.getInstructorName());
+                    courseOnline.setTotal_student(updatedCourse.getTotal_student());
+
+                    return this.courseOnlineRepository.save(courseOnline);
+
+                });
     }
 }
